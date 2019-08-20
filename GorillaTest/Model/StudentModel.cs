fd56702fb95987455f7c6efe1736d690a9b98502
Calo10 <carlos_mendez_10@live.com>
2019-08-20 15:11:48 -0600

    Api calls

--- GorillaTest/Model/StudentModel.cs
+++ GorillaTest/Model/StudentModel.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace GorillaTest.Model
 {
     public class StudentModel
@@ -38,6 +44,44 @@ namespace GorillaTest.Model
             get;
             set;
         }
+
+        public async static Task<ObservableCollection<StudentModel>> GetAllStudents()
+        {
+      
+            using (HttpClient client = new HttpClient())
+            {
+
+                var uri = new Uri("http://7dd91533.ngrok.io/api/values");
+
+                HttpResponseMessage response = await client.GetAsync(uri).ConfigureAwait(false);
+                string ans = await response.Content.ReadAsStringAsync();
+                var lstStudents = JsonConvert.DeserializeObject<ObservableCollection<StudentModel>>(ans);
+                return lstStudents;
+            }
+
+          
+        }
+
+        public async static Task<bool> AddStudent(StudentModel studentModel)
+        {
+           
+            using (HttpClient client = new HttpClient())
+            {
+                var uri = new Uri("http://7dd91533.ngrok.io/api/values");
+
+                var json = JsonConvert.SerializeObject(studentModel);
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(uri, content).ConfigureAwait(false);
+                string ans = await response.Content.ReadAsStringAsync();
+
+                //bool respuesta = JsonConvert.DeserializeObject<bool>(ans);
+
+                return true;
+            }
+            
+        }
+
 
     }
 }
